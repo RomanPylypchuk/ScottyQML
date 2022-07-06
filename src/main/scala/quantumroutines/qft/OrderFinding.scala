9d5a810dc7609ff0261451bf2c7c097a174afb00
RomanPylypchuk <rom_cs@ua.fm>
2022-07-06 16:32:12 +0300

    Estimate s/r from measurement using continued fractions

--- src/main/scala/quantumroutines/qft/OrderFinding.scala
+++ src/main/scala/quantumroutines/qft/OrderFinding.scala
@@ -7,8 +7,11 @@ import quantumroutines.qft.PhaseEstimation.phaseEstimate
 import scotty.quantum.ExperimentResult.StateStats
 import scotty.quantum.{BitRegister, Circuit}
 import spire.math.Rational
+import utils.MathOps.rationalToContinuedFractions
 import utils.codec.BiCodec.BiCodecSyntax
 import utils.factory.BitRegisterFactory.decimalBitRegister
+
+import scala.util.Random
 
 object OrderFinding {
 
@@ -34,6 +37,11 @@ object OrderFinding {
       val phaseDecimal: Int = BitRegister(phaseBinary.values.reverse :_*).decodeE[Int, Int](nPhaseQubits)
       val estimate: Rational = Rational(phaseDecimal, math.pow(2, nPhaseQubits).toInt)
 
-      "yo".invalidNec
+      val continuedFractions: List[Long] = estimate.decodeE[Option[Int], List[Long]](None)
+      val takeConvergent: Int = Random.nextInt(continuedFractions.length)
+      val convergentRational: Rational = continuedFractions.encodeE[Option[Int], Rational](Some(takeConvergent))
+      if ((estimate - convergentRational).toDouble <= Rational(1, math.pow(2, (nPhaseQubits - 1) / 2).toInt).toDouble)
+        convergentRational.validNec
+      else ("Randomly chosen convergent " + convergentRational + " for estimate " + estimate + "doesn't satisfy the conditions").invalidNec
   }
 }
