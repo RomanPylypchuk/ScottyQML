cca24bac115c8378ec342f0f8982d67f1d71adb3
RomanPylypchuk <rom_cs@ua.fm>
2022-11-16 21:45:49 +0200

    Inverse Gate and Inverse Circuit utils implementation

--- src/main/scala/utils/GateUtils.scala
+++ src/main/scala/utils/GateUtils.scala
@@ -1,18 +1,19 @@
 package utils
 
+import scotty.quantum.Circuit
 import scotty.quantum.gate.StandardGate.{CNOT, H, X}
-import scotty.quantum.gate.TargetGate
+import scotty.quantum.gate._
 
 object GateUtils {
 
-  val placeSingleQubitGate : (Int => TargetGate) => List[Int] => List[TargetGate] =
+  val placeSingleQubitGate: (Int => TargetGate) => List[Int] => List[TargetGate] =
     gateGen => indices => indices.map(gateGen)
 
   val placeHs: List[Int] => List[TargetGate] = placeSingleQubitGate(H.apply)
   val placeNOTs: List[Int] => List[TargetGate] = placeSingleQubitGate(X.apply)
   val placeCNOTs: Map[Int, List[Int]] => List[CNOT] = {
     controlsTargets =>
-      val cNOTs = for{
+      val cNOTs = for {
         (ci, tis) <- controlsTargets
         ti <- tis
       } yield CNOT(ci, ti)
@@ -20,9 +21,24 @@ object GateUtils {
   }
   val singlePlaceCNOTs: Map[Int, Int] => List[CNOT] = {
     controlTargetMap =>
-      placeCNOTs(controlTargetMap.map{case (ci, ti) => ci -> List(ti)})
+      placeCNOTs(controlTargetMap.map { case (ci, ti) => ci -> List(ti) })
   }
 
   def HTensor(n: Int): List[TargetGate] = placeHs((0 until n).toList)
   def XTensor(n: Int): List[TargetGate] = placeNOTs((0 until n).toList)
+
+  val inverseGate: PartialFunction[Gate, Gate] = {
+    case tg: TargetGate => Dagger(tg)
+    case sg: SwapGate => sg
+    case cg: ControlGate =>
+      val c = Controlled(cg.controlIndex, inverseGate(cg.target))
+      c : ControlGate
+  }
+
+  implicit class InverseCircuit(val circuit: Circuit) {
+    def inverse: Circuit = {
+       val inverseGates = circuit.gates.map(inverseGate)
+       Circuit(inverseGates.reverse :_*)
+    }
+  }
 }
---
+++ src/test/scala/utils/GateUtilsTest.scala
@@ -0,0 +1,44 @@
+package utils
+
+import scotty.quantum.gate.StandardGate._
+import scotty.quantum.{BitRegister, Circuit}
+import utils.GateUtils.{InverseCircuit, inverseGate}
+import utils.Measure.measureTimes
+import utils.codec.BiCodec.BiCodecSyntax
+import utils.factory.BitRegisterFactory.stringBitRegister
+
+object GateUtilsTest extends App{
+
+  //Inverse of TargetGate
+  val xAtZero = X(0)
+  val xInverse = inverseGate(xAtZero)
+
+  val xStats = measureTimes(1000)(Circuit(xAtZero, xInverse))
+  assert(xStats.stats.toMap.get("0".encode[BitRegister]).contains(1000))
+
+  //Inverse of SwapGate
+  val swapZeroOne = SWAP(0,1)
+  val inverseSwapZeroOne = inverseGate(swapZeroOne)
+  val swapStats = measureTimes(1000)(Circuit(X(0), swapZeroOne, inverseSwapZeroOne))
+  assert(swapStats.stats.toMap.get("01".encode[BitRegister]).contains(1000))
+
+  //Inverse of ControlGate
+
+  //CNOT
+  val cNotZeroOne = CNOT(0,1)
+  val inverseCNOTZeroOne = inverseGate(cNotZeroOne)
+  val cNOTStats = measureTimes(1000)(Circuit(X(0), cNotZeroOne, inverseCNOTZeroOne))
+  assert(cNOTStats.stats.toMap.get("01".encode[BitRegister]).contains(1000))
+
+  //CSWAP(Fredkin)
+  val fredkinZeroOneTwo = CSWAP(0,1,2)
+  val inverseFredkin = inverseGate(fredkinZeroOneTwo)
+  val cSwapStats = measureTimes(1000)(Circuit(X(0), fredkinZeroOneTwo))
+  assert(cSwapStats.stats.toMap.get("001".encode[BitRegister]).contains(1000))
+
+  //Inverse Circuit test
+  val qc = Circuit(X(0), Z(1), CNOT(0,1), SWAP(0,1), CNOT(1,0), Z(0))
+  val inverseQc = qc.inverse
+
+  println(measureTimes(1000)(qc combine inverseQc))
+}
