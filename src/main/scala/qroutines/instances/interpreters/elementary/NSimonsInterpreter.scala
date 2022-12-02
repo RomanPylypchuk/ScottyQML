db1eb1384b1d42fcbb05f378a227c89682d36f2d
RomanPylypchuk <rom_cs@ua.fm>
2022-12-02 10:07:35 +0200

    NSimonsInterpreter as QuantumRoutineInterpreter and brute force BinarySolver

--- src/main/scala/qroutines/instances/interpreters/elementary/NSimonsInterpreter.scala
+++ src/main/scala/qroutines/instances/interpreters/elementary/NSimonsInterpreter.scala
@@ -5,28 +5,31 @@ import cats.data.{Reader, ValidatedNec}
 import cats.implicits.catsSyntaxValidatedIdBinCompat0
 import qroutines.blocks.measurements.MeasurementResult
 import qroutines.blocks.routine.QuantumRoutineInterpreter
-import qroutines.blocks.routine.QuantumRoutineOutput.BitStringOutput
+import qroutines.blocks.routine.QuantumRoutineOutput.{BitStringOutput, OneOrTwoToOne, OneToOne, TwoToOne}
 import quantumroutines.blocks.CircuitParams.NumberQubits
 import quantumroutines.oracle.OracleOutput.VectorOutput
 import scotty.quantum.BitRegister
+import utils.BinarySolver.binarySolverBruteForce
 import utils.codec.BiCodec.BiCodecSyntax
-import utils.factory.BitRegisterFactory.stringBitRegister
+import utils.factory.BitRegisterFactory.{bitBitRegister, stringBitRegister}
 
-object NSimonsInterpreter extends QuantumRoutineInterpreter{
+object NSimonsInterpreter extends QuantumRoutineInterpreter {
   type InParamsType = NumberQubits
-  type RoutineOutput = BitStringOutput
+  type RoutineOutput = OneOrTwoToOne
 
-  val interpret: Reader[NumberQubits, MeasurementResult => ValidatedNec[String, BitStringOutput]] =
-    Reader{ case NumberQubits(nQubits) => {
+  val interpret: Reader[NumberQubits, MeasurementResult => ValidatedNec[String, OneOrTwoToOne]] =
+    Reader { case NumberQubits(nQubits) => {
       case MeasurementResult(neededQubitStats, _) =>
         val nonZeroDichotomies = neededQubitStats.stats.filter { case (_, occs) => occs != 0 }
-        //assert(nonZeroDichotomies.length >= function.nOracleQubits)
         val mostProbable = nonZeroDichotomies.sortBy(_._2).map(_._1).takeRight(nQubits / 2)
-        //val outcomesMatrix: DenseMatrix[Int] = DenseMatrix(mostProbable.map(br => br.decode[List[Int]]): _*)
+        val outcomesMatrix: DenseMatrix[Int] = DenseMatrix(mostProbable.map(br => br.decode[List[Int]]): _*)
+        val maybeOracleBits: Option[BitRegister] = binarySolverBruteForce(outcomesMatrix) //TODO - replace with decent solver
 
-        //TODO - actually need to solve the system of linear equations in binary variables
-        val b: BitRegister = "00".encode[BitRegister]
-        BitStringOutput(b).validNec
+          val result: ValidatedNec[String, OneOrTwoToOne] = maybeOracleBits.fold("Could not solve binary system".invalidNec[OneOrTwoToOne]){
+            case br if br.decode[List[Int]].forall(_ == 0) =>  OneToOne.validNec[String]
+            case br => TwoToOne(br).validNec[String]
+          }
+        result
      }
     }
 }
